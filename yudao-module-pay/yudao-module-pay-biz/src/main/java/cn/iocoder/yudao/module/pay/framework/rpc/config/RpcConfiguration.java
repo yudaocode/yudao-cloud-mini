00a9a66514b538f0dde5a4c63393857856259c81
YunaiV <zhijiantianya@gmail.com>
2024-05-01 10:12:31 +0800

    【同步】对齐 boot 和 cloud 的逻辑

--- yudao-module-pay/yudao-module-pay-biz/src/main/java/cn/iocoder/yudao/module/pay/framework/rpc/config/RpcConfiguration.java
+++
@@ -1,7 +0,0 @@
-package cn.iocoder.yudao.module.pay.framework.rpc.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration(proxyBeanMethods = false)
-public class RpcConfiguration {
-}
